74a305459f6cd877f489e4d06421120a6a4be320
ferxo098p <ferxo98skate@gmail.com>
2019-06-26 12:26:40 -0500

    Add files via upload

--- EjemploCSSjs/js/progMultimedia.js
+++ EjemploCSSjs/js/progMultimedia.js
@@ -1,29 +1,44 @@
+//Obtenemos el video del HTML y lo guardamos en una variable
 var video = document.getElementById("video1");
 
+//Esta funcíon nos regresa una página en el historial
 function volver(){
 	history.go(-1);
-}
+};
+
 
 function play(){
+	//Haciendo uso del método play para audio/video reproducimos el video
 	video1.play();
-}
+};
 
 function pause(){
+	//Haciendo uso del método pause para audio/video pausamos el video
 	video1.pause();
-}
+};
 
 function silenciar(){
+	/*Haciendo uso del método muted para audio/video silenciamos el video
+	true es para silenciar
+	false es para regresar el audio y que se vuelva a escuvhar*/
 	video1.muted = true;
-}
+};
 
 function sonido(){
+	/*Haciendo uso del método muted para audio/video silenciamos el video
+	true es para silenciar
+	false es para regresar el audio y que se vuelva a escuvhar*/
 	video1.muted = false;
-}
+};
 
 function aumentarVolumen(){
+	/*Haciendo uso del método volume para audio/video modificamos el volumen del vídeo,
+	dependiendo del valor dadod será lo alto/bajo que este sonará*/
 	video.volume = 1.0;
-}
+};
 
 function disminuirVolumen(){
+	/*Haciendo uso del método volume para audio/video modificamos el volumen del vídeo,
+	dependiendo del valor dadod será lo alto/bajo que este sonará*/
 	video.volume = 0.3;
-}
+};
